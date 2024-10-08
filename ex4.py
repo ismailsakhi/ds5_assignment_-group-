af1d2a646a7ea2a1a1727588a495291f9180fffa
IvanRstud <537389@student.fontys.nl>
2024-10-08 16:28:13 +0200

    ex4.1 is done

--- ex4.py
+++ ex4.py
@@ -1,4 +1,4 @@
-#ex4
+#ex4.1
 import pandas as pd
 from langdetect import detect, DetectorFactory
 from langdetect.lang_detect_exception import LangDetectException
@@ -7,11 +7,17 @@ from langdetect.lang_detect_exception import LangDetectException
 DetectorFactory.seed = 0
 
 # Load the Excel file containing the tweets
-file_path = 'tweets.xlsx'  # Ensure the file is in the current directory or provide the full path
-df = pd.read_excel(file_path)
+df = pd.read_excel('tweets.xlsx' )
 
 # Define a function to detect the language of each tweet
 def detect_language(tweet):
+    """
+    detecting the language of the messages 
+    input:
+        table with the messages
+    output:
+        column with the detected languges
+    """
     try:
         # Detect the language of the tweet
         return detect(tweet)
@@ -27,6 +33,12 @@ df['language'] = df['Tweet'].apply(detect_language)
 
 # Save the results back to an Excel file or display it
 df.to_excel('tweets_with_languages.xlsx', index=False)  # Save the output to a new Excel file
-print(df)
+
+#In the provided exel file there are a lot of messegas cosisting of the so-called Unicode 
+#for example some messages supposed to be in russian, however they were trascripted in "ÐŸÐ¾ÐµÑ…Ð°Ð»Ð° Ð½Ð° ÐºÐ°Ñ‚Ð¾Ðº"
+#which is not russian for sure (i'm a native speaker, so you can trust me). 
+#unfortunately,I've no idea how to fix it and it leads to some errors in language detection 
+
+
 
 
